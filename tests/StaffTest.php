49cef78738af1f2dcc96dc1ff2a48efde15e4bd1
codevuive85 <sgl3338888@gmail.com>
2022-08-16 20:21:33 +0700

    up load test case

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "Loi";
+        $staff->first_name = "sairoi";
         $this->assertEquals('Nguyen', $staff->getFullName());
     }
 }
